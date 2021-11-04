7a894783c00e2e23e7eff9f589a9614cc2c1ecd9
lkuoch <lkuoch@users.noreply.github.com>
2021-11-04 21:37:38 +1100

    feat: upgrade dependencies

--- src/components/customer/index.tsx
+++ src/components/customer/index.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import ListSubheader from "@mui/material/ListSubheader";
+
 import { actions, selectors } from "@features/customer/state";
 import { customerApi } from "@features/customer/api";
 
@@ -14,25 +20,26 @@ const Customer = () => {
   const currentCustomerId = useSelector(selectors.selectCurrentCustomerId);
 
   return (
-    <div id="customers" className="left-panel ui vertical menu">
-      <h2 className="title ui header">
-        <i className="ui user outline icon" />
-        <div className="content">Customers</div>
-      </h2>
-
+    <div id="customers">
       {isError && <h1>ERROR</h1>}
       {isLoading && <Loader />}
 
-      {isSuccess &&
-        entities.map(({ id, name: Name }) => (
-          <a
-            key={id}
-            className={`item ${id === currentCustomerId ? "red active" : ""}`}
-            onClick={() => (id !== currentCustomerId ? dispatch(actions.updateCurrentCustomerId(id)) : null)}
-          >
-            {Name}
-          </a>
-        ))}
+      <List
+        sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper", cursor: "pointer" }}
+        subheader={<ListSubheader component="div">Customers</ListSubheader>}
+      >
+        {isSuccess &&
+          entities.map(({ id, name }) => (
+            <ListItem
+              key={id}
+              selected={id === currentCustomerId}
+              onClick={() => (id !== currentCustomerId ? dispatch(actions.updateCurrentCustomerId(id)) : null)}
+            >
+              <ListItemAvatar></ListItemAvatar>
+              <ListItemText primary={name} />
+            </ListItem>
+          ))}
+      </List>
     </div>
   );
 };
